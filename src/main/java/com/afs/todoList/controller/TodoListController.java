3c3b94a0265f74e1725c6ecc992f3e5326c414f4
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-16 14:07:11 +0800

    init commit all necessary files

---
+++ src/main/java/com/afs/todoList/controller/TodoListController.java
@@ -0,0 +1,9 @@
+package com.afs.todoList.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("https://localhost:3000/")
+public class TodoListController {
+}
