ef57996bab3b6677039c4b55698a97783197d89c
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-16 14:16:33 +0800

    feat: create entity TodoItem

--- src/main/java/com/afs/todoList/controller/TodoListController.java
+++ src/main/java/com/afs/todoList/controller/TodoListController.java
@@ -1,9 +1,22 @@
 package com.afs.todoList.controller;
 
+import com.afs.todoList.entity.TodoItem;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("https://localhost:3000/")
+@RequestMapping("/todos")
 public class TodoListController {
+  public TodoListController() {
+  }
+
+  @GetMapping
+  public List<TodoItem> getAllTodos() {
+    return null;
+  }
 }
---
+++ src/main/java/com/afs/todoList/entity/TodoItem.java
@@ -0,0 +1,41 @@
+package com.afs.todoList.entity;
+
+import org.springframework.data.mongodb.core.mapping.FieldType;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+public class TodoItem {
+  @MongoId(FieldType.OBJECT_ID)
+  private String id;
+  private String content;
+  private boolean done;
+
+  public TodoItem(String id, String content, boolean done) {
+    this.id = id;
+    this.content = content;
+    this.done = done;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public boolean isDone() {
+    return done;
+  }
+
+  public void setDone(boolean done) {
+    this.done = done;
+  }
+}
