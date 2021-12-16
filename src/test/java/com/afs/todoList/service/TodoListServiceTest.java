4f10a172bdde9392b12b6f4c8870f026f0c8cbc2
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-16 19:20:20 +0800

    [unit test] test: add case should_delete_when_perform_DELETE_given_todo_id

--- src/test/java/com/afs/todoList/service/TodoListServiceTest.java
+++ src/test/java/com/afs/todoList/service/TodoListServiceTest.java
@@ -1,5 +1,6 @@
 package com.afs.todoList.service;
 
+import com.afs.todoList.advice.NoTodoFoundException;
 import com.afs.todoList.entity.TodoItem;
 import com.afs.todoList.repository.TodoListRepository;
 import org.junit.jupiter.api.Test;
@@ -9,11 +10,16 @@ import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 public class TodoListServiceTest {
@@ -49,6 +55,15 @@ public class TodoListServiceTest {
   }
 
   @Test
+  public void should_throw_exception_when_GET_given_not_exist_todo() {
+    TodoItem todoItem = new TodoItem("61bb1d02204e42bc87919ae8", "Do Something", false);
+    given(mockTodoListRepository.findById("61bb1d02204e42bc87919ae8"))
+      .willThrow(NoTodoFoundException.class);
+
+    assertThrows(NoTodoFoundException.class, () -> todoListService.findById(todoItem.getId()));
+  }
+
+  @Test
   public void should_create_todo_when_POST_given_new_todo() {
     TodoItem todoItemInput = new TodoItem("1", "Do Something", null);
     TodoItem todoItemReturned = new TodoItem("1", "Do Something", false);
@@ -79,5 +94,18 @@ public class TodoListServiceTest {
 
     assertEquals(todoItem.getContent(), actual.getContent());
     assertEquals(todoItem.getDone(), actual.getDone());
+  }
+
+  @Test
+  public void should_delete_when_perform_DELETE_given_todo_id() {
+    TodoItem todoItem = new TodoItem("1", "Do Something", true);
+
+    willDoNothing().given(mockTodoListRepository).delete(any(TodoItem.class));
+    given(mockTodoListRepository.findById(any()))
+      .willReturn(java.util.Optional.of(todoItem));
+
+    todoListService.delete(todoItem.getId());
+
+    verify(mockTodoListRepository).delete(any(TodoItem.class));
   }
 }
