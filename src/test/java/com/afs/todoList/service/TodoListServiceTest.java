22d068eb942a152cd2c96fd445e60ddac4ae74c4
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-16 18:41:13 +0800

    [unit test] test: add case should_get_all_todos_when_GET_given_todos

---
+++ src/test/java/com/afs/todoList/service/TodoListServiceTest.java
@@ -0,0 +1,37 @@
+package com.afs.todoList.service;
+
+import com.afs.todoList.entity.TodoItem;
+import com.afs.todoList.repository.TodoListRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(SpringExtension.class)
+public class TodoListServiceTest {
+  @Mock
+  TodoListRepository mockTodoListRepository;
+  
+  @InjectMocks
+  TodoListService todoListService;
+  
+  @Test
+  public void should_get_all_todos_when_GET_given_todos() {
+    List<TodoItem> todoItemList = new ArrayList<>();
+    todoItemList.add(new TodoItem(null, "Do something.", false));
+    given(mockTodoListRepository.findAll())
+      .willReturn(todoItemList);
+
+    List<TodoItem> actual = todoListService.findAll();
+
+    assertEquals(todoItemList.get(0).getContent(), actual.get(0).getContent());
+    assertEquals(todoItemList.get(0).getDone(), actual.get(0).getDone());
+  }
+}
